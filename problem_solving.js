5202d15a2fca10b6106a2f50b0350f415c3235f1
shanith <shanithsanu@gmail.come>
2025-06-19 08:02:04 -0700

    first commit

---
+++ problem_solving.js
@@ -0,0 +1,94 @@
+// const arr=[1,2,13,4];
+
+// let larg=0;
+
+// for(let i=0;i<arr.length;i++){
+//      if(larg<arr[i]){
+//         larg=arr[i]
+//      }
+// }
+
+// console.log(larg);
+
+
+
+//LARG NUMBER IN A ARRAY
+// -------------------------------------------------------------------------------------------------------------
+
+// const arr=[34,2,13,4];
+
+// let small=arr[0];
+
+// for(let i=0;i<arr.length;i++){
+//      if(small>arr[i]){
+//         small=arr[i]
+//      }
+// }
+
+// console.log(small);
+
+
+//SMALL NUMBER IN A ARRAY
+// -------------------------------------------------------------------------------------------------------------
+
+
+// const arr=[1,2,3,4];
+
+// let sum=0;
+
+// for(let i=0;i<arr.length;i++){
+//      sum+=arr[i]
+//      }
+
+
+// console.log(sum);
+
+
+//SUM OF THE ELEMENTS IN ARRAY
+// -------------------------------------------------------------------------------------------------------------
+
+// const arr=[1,2,13,4,78];
+
+// let end=arr.length-1;
+// let dummy=0;
+
+// for(let i=0;i<=end;i++){
+//     let k=end-i
+//           if(i<=k){
+//                dummy=arr[i]
+//                arr[i]=arr[k]
+//                arr[k]=dummy
+               
+          
+//      }
+// }
+
+// console.log(arr);
+
+//REVARSE OF ELEMENT
+// ----------------------------------------------------------------------------------------------------------------
+
+
+// const arr=[1,2,3,4,2,2,2,2,2,2,2,2,2,2,1];
+
+// let odd=0;
+// let even=0;
+
+// for(let i=0;i<arr.length;i++){
+    
+//     if(arr[i]%2==0){
+
+//         even++
+//     }else{
+
+//         odd++
+    
+//     }
+
+//      }
+
+
+// console.log(odd,even);
+
+// ODD OR EVEN
+// ----------------------------------------------------------------------------------------------------------------
